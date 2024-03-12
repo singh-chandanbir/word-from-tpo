f27951eb333bde1c2cd645eaa43dad93ab16cc23
chandanbir <chandanbirsingh1@gmail.com>
2024-03-12 22:49:18 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+
+
+module.exports = {
+  
+  content: [
+
+    "*.html",
+ 
+
+  ],
+  theme: {
+    extend: {
+      
+      fontFamily: {
+
+        // add the font family you want to use here
+        
+
+      },
+   
+    }
+
+  }}
